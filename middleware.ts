d2ddb568e7986900f04264aa52df28994e955ffe
城川勇汰 <shirokawa.yuta@gmail.com>
2025-07-28 21:32:39 +0900

    astro middleware追加

--- middleware.ts
+++
@@ -1,8 +0,0 @@
-export const config = {}
-
-export default function middleware(request: Request) {
-  console.log('on middleware')
-  console.log('URL:', request.url)
-  console.log('Method:', request.method)
-  console.log('Headers:', Object.fromEntries(request.headers.entries()))
-}
