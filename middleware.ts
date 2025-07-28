0499d6abb5b1faa218780432b7733a88f9abe22e
城川勇汰 <shirokawa.yuta@gmail.com>
2025-07-28 21:29:08 +0900

    wip

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,8 @@
-import { rewrite } from '@vercel/functions'
-
-export const config = {
-  matcher: '/example/posts/:path*',
-}
+export const config = {}
 
 export default function middleware(request: Request) {
   console.log('on middleware')
   console.log('URL:', request.url)
   console.log('Method:', request.method)
   console.log('Headers:', Object.fromEntries(request.headers.entries()))
-
-  // 1/2の確率で posts_ab ページにリライト
-  if (Math.random() < 0.5) {
-    console.log('hit!')
-    return rewrite(new URL('/example/posts_ab', request.url))
-  }
 }
