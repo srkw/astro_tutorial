4a10879601c3c7e887d8cdac55771534922cd62a
城川勇汰 <shirokawa.yuta@gmail.com>
2025-09-20 19:02:35 +0900

    add react integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 // @ts-check
-import preact from '@astrojs/preact'
+import react from '@astrojs/react'
 import vercel from '@astrojs/vercel'
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()],
+  integrations: [react()],
   adapter: vercel({
     edgeMiddleware: true,
     isr: {
