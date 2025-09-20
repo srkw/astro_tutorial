c2c0adb8ffa2bce7ed092f0dc3ef263fb236a042
城川勇汰 <shirokawa.yuta@gmail.com>
2025-09-21 00:15:04 +0900

    invalidate API

--- src/pages/api/invalidate-page.ts
+++ src/pages/api/invalidate-page.ts
@@ -15,37 +15,27 @@ export const GET: APIRoute = async ({ request }) => {
   })
 }
 
-// export const POST: APIRoute = () => {
-//   // console.log('Revalidation request received')
-//   // const body = await request.json()
-//   // const route = body.route ?? '/'
-//   // console.log(`Revalidating route: ${{ url, route }}`)
+export const POST: APIRoute = async ({ request }) => {
+  console.log('Revalidation request received')
+  const body = await request.json()
+  const route = body.route ?? '/'
+  const url = new URL(request.url)
+  console.log(`Revalidating route: ${{ url, route }}`)
 
-//   // const response = await fetch(`https://${url.host}${route}`, {
-//   //   method: 'HEAD',
-//   //   headers: {
-//   //     'x-prerender-revalidate': import.meta.env.REVALIDATE_TOKEN,
-//   //   },
-//   // })
+  const response = await fetch(`https://${url.host}${route}`, {
+    method: 'HEAD',
+    headers: {
+      'x-prerender-revalidate': import.meta.env.REVALIDATE_TOKEN,
+    },
+  })
 
-//   // const wasInvalidated =
-//   //   response.headers.get('X-Vercel-Cache') === 'REVALIDATED'
+  const wasInvalidated =
+    response.headers.get('X-Vercel-Cache') === 'REVALIDATED'
 
-//   // return new Response(
-//   //   JSON.stringify({
-//   //     status: 200,
-//   //     wasInvalidated,
-//   //   })
-//   // )
-//   const data = {
-//     message: 'POST Hello, World!',
-//     timestamp: new Date().toISOString(),
-//   }
-
-//   return new Response(JSON.stringify(data), {
-//     status: 200,
-//     headers: {
-//       'Content-Type': 'application/json',
-//     },
-//   })
-// }
+  return new Response(
+    JSON.stringify({
+      status: 200,
+      wasInvalidated,
+    })
+  )
+}
