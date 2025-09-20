ebdb371234f40f71d7c9b236f6f8fe088b06d423
城川勇汰 <shirokawa.yuta@gmail.com>
2025-09-20 19:04:01 +0900

    switch react to preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 // @ts-check
-import react from '@astrojs/react'
+import preact from '@astrojs/preact'
 import vercel from '@astrojs/vercel'
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [preact()],
   adapter: vercel({
     edgeMiddleware: true,
     isr: {
