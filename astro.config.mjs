55b1065d8d3f1186e0c98f663b274534116e1361
城川勇汰 <shirokawa.yuta@gmail.com>
2025-09-20 19:56:55 +0900

    add middleware config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,15 @@ import preact from '@astrojs/preact'
 import vercel from '@astrojs/vercel'
 import { defineConfig } from 'astro/config'
 
+export const config = {
+  matcher: ['/((?!api).*)'],
+}
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact()],
   adapter: vercel({
-    // edgeMiddleware: true,
+    edgeMiddleware: true,
     isr: {
       bypassToken: import.meta.env.REVALIDATE_TOKEN,
       exclude: [/^\/api\/.+/],
